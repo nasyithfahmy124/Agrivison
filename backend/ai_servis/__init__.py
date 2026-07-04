8034607d0e71d11dcd93ec0aed1b9f79c6ceb66e
Nasyith Fahmy <nasyithfahmy124@gmail.com>
2026-07-04 22:10:14 +0700

    Fix gitignore and remove large temporary files

--- backend/ai_servis/__init__.py
+++
@@ -1,1 +0,0 @@
-default_app_config = "ai_servis.apps.AiServisConfig"
--- backend/ai_servis/tests.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path,include
-from .views import DiseaseDetectionView, HistoryListView, HistoryDetailView
-from rest_framework.routers import DefaultRouter
-
-urlpatterns = [
-    path("api/detect/", DiseaseDetectionView.as_view(), name="disease-detection"),
-    path("api/history/", HistoryListView.as_view(), name="history-list"),
-    path("api/history/<int:pk>/", HistoryDetailView.as_view(), name="history-detail"),
-]
